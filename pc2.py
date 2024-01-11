2fc528de9a5034aea874969353b92926c056e437
Fernando Castell <fercasgdo11@gmail.com>
2024-01-11 13:55:10 +0100

    updated pc2.py -- quitado de cosas inutiles

--- pc2.py
+++ pc2.py
@@ -3,18 +3,15 @@
 # Grupo 21
 #!/usr/bin/python3
 
-
-#TODO: probar en GCP
 import os
 import subprocess
 
 # Puerto del servicio para google cloud
-puerto = 9080 #8080 #Use a port number above 1024
+puerto = 9080 #Use a port number above 1024 -- regla FW
 
 # Actualización del sistema
-os.system('sudo apt-get -y update') # os.system('sudo apt.get -y update')
-os.system('sudo apt-get -y upgrade') # os.system('sudo apt.get -y upgrade')
-
+os.system('sudo apt-get -y update')
+os.system('sudo apt-get -y upgrade')
 
 # Instalaciones necesarias: git, python, pip
 # Clonado de la practica
@@ -27,35 +24,13 @@ os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 # subprocess.check_call(["python3", "-m", "pip", "install", "-r", "./practica_creativa2/bookinfo/src/productpage/requirements.txt"])
 os.system('python3 -m pip install -r ./practica_creativa2/bookinfo/src/productpage/requirements.txt')
 
-# Da mazo por culo #TODO
+# Instalar -- no creo que haga falta pero funciona #TODO
 os.system('python3 -m pip install Flask-Bootstrap==3.3.7.1')
-
-# # Instalación de  librerias que dan problemas de versiones
-# os.system('pip install urllib3')
-# os.system('pip install flask_bootstrap')
-# os.system('pip install jaeger-client')
-# os.system('pip install opentracing-instrumentation')
 
 # Declaración de la variable de entorno <GRUPO_NUMERO>
 os.environ['GRUPO_NUMERO'] = '21'
 grupo_numero = os.getenv('GRUPO_NUMERO')
 
-
-# Modificación del título de la app para que aparezca el valor de la variable de entorno.
-# Para el archivo index.html
-
-# with open("./practica_creativa2/bookinfo/src/productpage/templates/index.html", "r") as f:
-#     code = f.read()
-# code = code.replace("Simple Bookstore App", str(grupo_numero))
-# with open("./practica_creativa2/bookinfo/src/productpage/templates/index.html", "w") as f:
-#     f.write(code)
-# # Para productpage.html
-# with open("./practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "r") as f:
-#     code = f.read()
-# code = code.replace("Simple Bookstore App", str(grupo_numero))
-# with open("./practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "w") as f:
-#     f.write(code)
-    
 # Modificar el título en el index.html y en productpage.html
 # VARIABLE DE ENTORNO
 ruta_pp = "./practica_creativa2/bookinfo/src/productpage/templates/productpage.html"

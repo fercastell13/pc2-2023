2cf28e5934a1c445300f675f7ad975bfb2e0c48a
Fernando Castell <fercasgdo11@gmail.com>
2024-01-11 13:47:59 +0100

    puertyo 9080

--- pc2.py
+++ pc2.py
@@ -9,7 +9,7 @@ import os
 import subprocess
 
 # Puerto del servicio para google cloud
-puerto = 80 #8080 #Use a port number above 1024
+puerto = 9080 #8080 #Use a port number above 1024
 
 # Actualización del sistema
 os.system('sudo apt-get -y update') # os.system('sudo apt.get -y update')

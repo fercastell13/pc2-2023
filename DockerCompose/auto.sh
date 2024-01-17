186227ce16c1cd1e7b19c1ac82df2858c189198f
Fernando Castell <fercasgdo11@gmail.com>
2024-01-17 19:13:04 +0100

    parte 3 hecha -- DONE

--- DockerCompose/auto.sh
+++ DockerCompose/auto.sh
@@ -10,8 +10,17 @@
 docker build -t 21/productpage -f ./ProductPage/Dockerfile .
 docker build -t 21/details -f ./Details/Dockerfile .
 
-# docker build -t 21/reviews -f ./Reviews/Dockerfile .
-# docker build -t 21/ratings -f ./Ratings/Dockerfile .
+# Para Reviews:
+# copiar los ficheros de la aplicacion
+# cambiar pwd por PWD en el siguiente comando: (powershell)
+# docker run --rm -u root -v ${PWD}:/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build
+docker build -t 21/reviews -f ./Reviews/reviews-wlpcfg/Dockerfile .
+
+# docker build -t 21/reviews:v3 -f ./Reviews/reviews-wlpcfg/Dockerfile .
+
+
+
+docker build -t 21/ratings -f ./Ratings/Dockerfile .
 
 # Lanzar los contenedores con Docker Compose
 docker-compose up

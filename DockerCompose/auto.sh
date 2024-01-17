ddbe2f6ae219d4c98b29efcef60fc422cf0406b8
Fernando Castell <fercasgdo11@gmail.com>
2024-01-17 13:54:56 +0100

    parte 3.2 -- ONGOING
    Co-authored-by: belencasajus <belencasajus@users.noreply.github.com>

--- DockerCompose/auto.sh
+++ DockerCompose/auto.sh
@@ -8,9 +8,10 @@
 
 # Construir las imágenes
 docker build -t productpage -f ./ProductPage/Dockerfile .
-# docker build -t details -f ./Ratings/Dockerfile .
-# docker build -t details -f ./Reviews/Dockerfile .
-# docker build -t details -f ./Ratings/Dockerfile .
+docker build -t details -f ./Details/Dockerfile .
+# docker build -t details -f ./DockerCompose/Details/Dockerfile .
+# docker build -t reviews -f ./Reviews/Dockerfile .
+# docker build -t ratings -f ./Ratings/Dockerfile .
 
 # Lanzar los contenedores con Docker Compose
 docker-compose up

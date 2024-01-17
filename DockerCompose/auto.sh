bf15673255bd43edb60e552fa55ff0010053b193
Fernando Castell <fercasgdo11@gmail.com>
2024-01-17 11:59:56 +0100

    context yaml, auto.sh prueba product-page

--- DockerCompose/auto.sh
+++ DockerCompose/auto.sh
@@ -1,15 +1,16 @@
 # ./auto.sh
 
+# Ejecutar desde la carpeta de DockerCompose
 # En caso de no tener permisos de escritura, ejecutar en linux:
 # chmod +x auto.sh
 # o en windows:
 # icacls auto.sh /grant "usrname:RX"
 
 # Construir las imágenes
-docker build -t product-page -f ./DockerCompose/ProductPage/Dockerfile .
-docker build -t details -f ./DockerCompose/Ratings/Dockerfile .
-docker build -t details -f ./DockerCompose/Reviews/Dockerfile .
-docker build -t details -f ./DockerCompose/Ratings/Dockerfile .
+docker build -t product-page -f ./ProductPage/Dockerfile .
+# docker build -t details -f ./Ratings/Dockerfile .
+# docker build -t details -f ./Reviews/Dockerfile .
+# docker build -t details -f ./Ratings/Dockerfile .
 
 # Lanzar los contenedores con Docker Compose
 docker-compose up

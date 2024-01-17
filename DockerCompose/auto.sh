4c0b328d40826c9b21c9c81387d84684054f2a59
Fernando Castell <fercasgdo11@gmail.com>
2024-01-17 17:05:39 +0100

    PARTE 3.3 DONE -- DETAILS

--- DockerCompose/auto.sh
+++ DockerCompose/auto.sh
@@ -7,13 +7,13 @@
 # icacls auto.sh /grant "usrname:RX"
 
 # Construir las imágenes
-docker build -t productpage -f ./ProductPage/Dockerfile .
-docker build -t details -f ./Details/Dockerfile .
+docker build -t 21/productpage -f ./ProductPage/Dockerfile .
+# docker build -t 21/details -f ./Details/Dockerfile .
 # docker build -t g40/details .
 # docker build -t details -f ./DockerCompose/Details/Dockerfile .
 
-# docker build -t reviews -f ./Reviews/Dockerfile .
-# docker build -t ratings -f ./Ratings/Dockerfile .
+# docker build -t 21/reviews -f ./Reviews/Dockerfile .
+# docker build -t 21/ratings -f ./Ratings/Dockerfile .
 
 # Lanzar los contenedores con Docker Compose
 docker-compose up
